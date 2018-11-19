152d465a11a3ff81912b145e9c42cf20b85029cf
mareana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-11-19 16:36:53 -0300

    1a questao funcionando

--- src/questao_1/Main.java
+++ src/questao_1/Main.java
@@ -1,23 +1,59 @@
 package questao_1;
 
+public class Main {
 
-public class Main{ 
-	
-	public static void main(String[] args) {
-		Canal canal = new Canal(5);
-		new Thread(t1).start();
-		new Thread(t2).start();
+	private static Canal canal;
+
+	public static void main(String[] args) throws InterruptedException {
+		canal = new Canal(5);
+		Thread t1 = new Thread(produces);
+		Thread t2 = new Thread(consumes);
+		t1.start();
+		t2.start();
+		
+		t1.join();
+		t2.join();
+		
+//		System.out.println(canal.getCount());
+		
 	}
 
-	private static Runnable t1 = new Runnable() {
+	private static Runnable consumes = new Runnable() {
 		public void run() {
-
+//			while (true) {
+				synchronized (canal) {
+					try {
+						canal.takeMessage();
+						canal.takeMessage();
+//						canal.takeMessage();
+//						canal.takeMessage();
+//						canal.takeMessage();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+//				}
+			}
 		}
 	};
 
-	private static Runnable t2 = new Runnable() {
+	private static Runnable produces = new Runnable() {
 		public void run() {
+//			while (true) {
+				// sincronizando o dado que será usado, região crítica.
+				synchronized (canal) {
+					try {
+						canal.putMessage("oi garouta");
+						canal.putMessage("alo");
+//						canal.putMessage("das");
+//						canal.putMessage("odasie");
+//						canal.putMessage("oidasde");
+//						canal.putMessage("oieaaaaa");
 
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+//			}
 		}
 	};
 }
