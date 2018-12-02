14e7ddc32ab2f1332d6f8ca802a9012536e0c4b9
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-12-02 20:36:02 -0300

    Experimento questao 5 letra b

--- src/questao_5/letra_b/Experiment.java
+++ src/questao_5/letra_b/Experiment.java
@@ -1,20 +1,21 @@
 package questao_5.letra_b;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Experiment {
-	static final int nIteration = 10;
-	static final int nThreads = 2;
+	static final int nIteration = 10000;
+	static final int nThreads = 4;
 
-	static Long testMap(Map<String, Integer> map) throws InterruptedException{
+	static Long testMap(List<Integer> list) throws InterruptedException{
         CountDownLatch latch = new CountDownLatch(nThreads);
         long startTime = System.currentTimeMillis();
             ExecutorService exec = Executors.newFixedThreadPool(nThreads);
             for(int i = 0; i < nThreads; i++)
-                exec.submit(new Main(map, nIteration, latch));
+                exec.submit(new Main(list, nIteration, latch));
             latch.await();  
             exec.shutdown();
         long endTime = System.currentTimeMillis();
--- src/questao_5/letra_b/Main.java
+++ src/questao_5/letra_b/Main.java
@@ -6,26 +6,18 @@ import java.util.*;
 
 public class Main implements Runnable {
 	private static Experiment exp;
-    private Map<String, Integer> map;
+    private List<Integer> list;
     private static int index;
     private int numInterations; 
     private Random random = new Random();
     private final CountDownLatch latch;
     
 
-    public Main(Map<String, Integer> map, int numInterations, CountDownLatch latch) {
-        this.map = map;
+    public Main(List<Integer> list, int numInterations, CountDownLatch latch) {
+        this.list = list;
         this.numInterations = numInterations;
         this.latch = latch;
         this.exp = new Experiment();
-    }
-
-    private synchronized String getNextString() {
-        byte[] array = new byte[7];
-        this.random.nextBytes(array);
-        String generatedString = new String(array);
-        
-        return generatedString;
     }
 
     private synchronized int getNextInt() { return index++; }
@@ -34,8 +26,8 @@ public class Main implements Runnable {
     public void run() {
         while(numInterations > 0) {
         	int nextInt = getNextInt();
-            map.put(getNextString(), nextInt);
-            System.out.println("Insert " + nextInt + " into " + this.map.getClass().getSimpleName());
+        	list.add(nextInt);
+//            System.out.println("Insert " + nextInt + " into " + this.map.getClass().getSimpleName());
             numInterations--;
         }
         latch.countDown();
@@ -46,18 +38,20 @@ public class Main implements Runnable {
         ArrayList<Long> syncResult = new ArrayList<Long>(); 
         ArrayList<Long> concurrentMapResult = new ArrayList<Long>(); 
 
-        Map<String, Integer> syncMap = Collections.synchronizedMap(new HashMap<String, Integer>());
-        Map<String, Integer> concurrent = new ConcurrentHashMap<>();
+        List list = new ArrayList();
+        List syncList = Collections.synchronizedList(list);
+        
+        List copyOnWrite = new CopyOnWriteArrayList();
 
         System.out.println("Start insert...");
         
         //Rodar 5 vezes as n iterações definidas em Experiment;
         for(int i = 0; i < 5; i++) {
-            syncResult.add(exp.testMap(syncMap)); 
-            concurrentMapResult.add(exp.testMap(concurrent));
+            syncResult.add(exp.testMap(syncList)); 
+            concurrentMapResult.add(exp.testMap(copyOnWrite));
         }
-        System.out.println(" Collections.synchronizedMap -->  time difference: " + syncResult +  " milliseconds");
-        System.out.println(" ConcurrentMap               -->  time difference: " + concurrentMapResult +  " milliseconds");
+        System.out.println(" Collections.synchronizedList -->  time difference: " + syncResult +  " milliseconds");
+        System.out.println(" CopyOnWriteArrayList         -->  time difference: " + concurrentMapResult +  " milliseconds");
 
 
     }
