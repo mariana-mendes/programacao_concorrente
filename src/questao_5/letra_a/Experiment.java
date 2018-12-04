212677e77140de30cb6e001fdad42fb0690fed44
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2018-12-03 23:39:12 -0300

    5a alterada

--- src/questao_5/letra_a/Experiment.java
+++ src/questao_5/letra_a/Experiment.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Executors;
 
 public class Experiment {
 	static final int inserts = 1;
-	static final int nThreads = 1000;
+	static int nThreads = 2;
 	
     public static void main(String[] args) throws InterruptedException {
         ArrayList<Long> syncResultPut = new ArrayList<Long>(); 
@@ -19,16 +19,22 @@ public class Experiment {
         
         ArrayList<Long> syncResultGet = new ArrayList<Long>(); 
         ArrayList<Long> concurrentMapResultGet = new ArrayList<Long>(); 
+        ArrayList<Integer> numOfThreads = new ArrayList<Integer>(); 
 
         Map<String, Integer> syncMap = Collections.synchronizedMap(new HashMap<String, Integer>());
         Map<String, Integer> concurrent = new ConcurrentHashMap<>();
 
-        for(int i = 0; i < 5; i++) {
-        	syncResultPut.add(testMap(syncMap)); 
-        	concurrentMapResultPut.add(testMap(concurrent));
-        	syncResultGet.add(testMapGet(syncMap));
-        	concurrentMapResultGet.add(testMap(concurrent));
+        for(int i = 0; i < 10; i++) {
+        	syncResultPut.add(testMap(syncMap, nThreads)); 
+        	concurrentMapResultPut.add(testMap(concurrent, nThreads));
+        	syncResultGet.add(testMapGet(syncMap, nThreads));
+        	concurrentMapResultGet.add(testMap(concurrent, nThreads));
+        	numOfThreads.add(nThreads);
+            nThreads*=2;
         }
+        
+        System.out.println(" Number of Threads           -->                   " + numOfThreads );
+        
         System.out.println(" Collections.synchronizedMap -->  time difference: " + syncResultPut +  " milliseconds");
         System.out.println(" ConcurrentMap               -->  time difference: " + concurrentMapResultPut +  " milliseconds");
         
@@ -36,7 +42,7 @@ public class Experiment {
         System.out.println(" ConcurrentMapGet               -->  time difference: " + concurrentMapResultGet +  " milliseconds");
     }
 
-	static Long testMap(Map<String, Integer> map) throws InterruptedException{
+	static Long testMap(Map<String, Integer> map, int nThreads) throws InterruptedException{
         CountDownLatch latch = new CountDownLatch(nThreads);
 
         long startTime = System.currentTimeMillis();
@@ -50,7 +56,7 @@ public class Experiment {
         return (endTime - startTime);
     }
 	
-	static Long testMapGet(Map<String, Integer> map) throws InterruptedException{
+	static Long testMapGet(Map<String, Integer> map, int nThreads) throws InterruptedException{
         long startTime = System.currentTimeMillis();
             ExecutorService exec = Executors.newFixedThreadPool(nThreads);
             for(int i = 0; i < nThreads; i++) {
